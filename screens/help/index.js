25e4a186dc7b2f125d50083afc67939fd80b37ce
amit singh <amit@asparia.com>
2022-08-19 15:01:51 +0530

    removed unused code in htlp

--- screens/help/index.js
+++ screens/help/index.js
@@ -20,25 +20,8 @@ const Index = () => {
         setTicketId(ticketId);
     }
 
-    const TabsOld = () => {
-        return (
-            <>
-                <Pressable onPress={()=>handleChangeTab(HELP_TAB_TYPE.MY_TICKET)} style={{...indexStyles.TAB_BTN, ...(activeTab === HELP_TAB_TYPE.MY_TICKET ? indexStyles.TAB_BTN_ACTIVE : {})}}>
-                        <Text style={{ ...indexStyles.TAB_BTN_TEXT, ...(activeTab === HELP_TAB_TYPE.MY_TICKET ? indexStyles.TAB_BTN_TEXT_ACTIVE : {}) }}>My Tickets</Text>
-                    </Pressable>
-    
-                <Pressable onPress={()=>handleChangeTab(HELP_TAB_TYPE.LIVE_CHAT)} style={{...indexStyles.TAB_BTN, ...(activeTab === HELP_TAB_TYPE.LIVE_CHAT ? indexStyles.TAB_BTN_ACTIVE : {})}}>
-                    <Text style={{ ...indexStyles.TAB_BTN_TEXT, ...(activeTab === HELP_TAB_TYPE.LIVE_CHAT ? indexStyles.TAB_BTN_TEXT_ACTIVE : {}) }}>Live Chat</Text>
-                </Pressable>
-            </>
-        )
-    }
-
     return (
         <View style={indexStyles.CONTAINER}>
-            {/* <View style={indexStyles.BOARD_ROW}>
-                <Tabs/>
-            </View> */}
             <Tabs tabList={ HELP_TABS } activeTab = {activeTab} setActiveTab={handleChangeTab}/>
 
             {
